d3091b5b48c3cf0efdf68e84d01799eea5631207
Douglas <douglasilvadev@gmail.com>
2021-11-05 11:48:08 -0300

    Improving mail service

--- webview.js
+++ webview.js
@@ -1,7 +1,17 @@
+const _path = _interopRequireDefault(require('path'));
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
 module.exports = Ferdi => {
   const getMessages = function getMessages() {
-    const directMessages = document.getElementsByClassName('buffer conversation active unread activeBadge').length;
-    const indirectMessages = document.getElementsByClassName('buffer channel active unread').length;
+    const directMessages = document.getElementsByClassName(
+      'buffer conversation active unread activeBadge'
+    ).length;
+    const indirectMessages = document.getElementsByClassName(
+      'buffer channel active unread'
+    ).length;
 
     Ferdi.setBadge(directMessages, indirectMessages);
   };
